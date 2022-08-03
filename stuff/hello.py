e62444da56fa78f90b920bd836999b4478373be4
Natasha Kabasa <natashakabasa19@gmail.com>
2022-08-03 13:08:06 -0400

    third commit

--- stuff/hello.py
+++ stuff/hello.py
@@ -1,7 +1,53 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 #Create a flask instance 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+#Create a Form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+    #BooleanField
+    #DateField
+    #DateTimeField
+    #DecimalField
+    #FileField
+    #HiddenField
+    #MultipleField
+    #FieldList
+    #FloatField
+    #FormField
+    #IntegerField
+    #PasswordField
+    #RadioField
+    #SelectField
+    #SelectMultipleField
+    #SubmitField
+    #StringField
+    #TextAreaField
+
+    ##Validators
+    #DataRequired
+    #Email
+    #EqualTo
+    #InputRequired
+    #IPAddress
+    #Length
+    #MacAddress
+    #NumberRange
+    #Optional
+    #Regexp
+    #URL
+    #UUID
+    #AnyOf
+    #NoneOf
+
 
 #Create  a route decorator
 @app.route('/')
@@ -44,3 +90,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+#Create name page 
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html",
+        name = name,
+        form = form)
